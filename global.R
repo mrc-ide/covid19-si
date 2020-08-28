0004f1cb94e22faa576025220814dc921a0b71d5
sangeeta <sangeetabhatia03@gmail.com>
2020-08-28 16:54:11 +0100

    Latest estimates of incubation and infectious periods

---
+++ global.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(rstan)
+library(drake)
+library(hermione)
+max_shed <- 21
+
+## https://doi.org/10.1016/S1473-3099(20)30287-5
+mean_inf <- 6.3 ## days
+sd_inf <- 4.2 ## days
+params_inf <- epitrix::gamma_mucv2shapescale(mean_inf, sd_inf/ mean_inf)
+## https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7014672/
+mean_inc <- 6.5 ## days
+sd_inc <- 2.6 ## days
+params_inc <- epitrix::gamma_mucv2shapescale(mean_inc, sd_inc/ mean_inc)
