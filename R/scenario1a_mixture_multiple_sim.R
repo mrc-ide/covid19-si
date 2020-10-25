da9ae58dfb98711ccafb78478e87ec9ac84ecdc8
sangeeta <sangeetabhatia03@gmail.com>
2020-10-25 11:12:31 +0000

    Somewhat hacky code, gets the job done

--- R/scenario1a_mixture_multiple_sim.R
+++ R/scenario1a_mixture_multiple_sim.R
@@ -69,7 +69,7 @@ map_estimates <- function(fit) {
   map(fitted_params, ~ .[map_idx])
 }
 
-simulated_data <- pmap(
+out <- pmap(
   param_grid,
   function(params_inf, params_inc, params_pinv) {
     outfile <- paste(
@@ -92,6 +92,9 @@ simulated_data <- pmap(
     ggsave(glue::glue("figures/{outfile}.png"), p)
 
     simulated_si <- simulated_si[[3]]
+
+    saveRDS(simulated_si, glue::glue("stanfits/simulated_{outfile}.rds"))
+
     width <- min(simulated_si[simulated_si > 0]) / 2
     params2_inc <- gamma_mucv2shapescale(
       params_inc$mean_inc, params_inc$sd_inc/ params_inc$mean_inc
@@ -127,6 +130,75 @@ simulated_data <- pmap(
     saveRDS(
       posterior_si, glue::glue("stanfits/posterior_si_{outfile}.rds")
     )
+
+    p2 <- ggplot() +
+      geom_density(aes(simulated_si, fill = "blue"), alpha = 0.3) +
+      geom_density(aes(posterior_si[[3]], fill = "red"), alpha = 0.3) +
+      scale_fill_identity(
+        guide = "legend",
+        labels = c("Simulated", "Posterior"),
+        breaks = c("blue", "red")
+      ) +
+      xlab("Serial Interval") +
+      ylab("Probability Density") +
+      theme_minimal() +
+      theme(legend.title = element_blank())
+
+    ggsave(glue::glue("figures/posterior_{outfile}.png"), p2)
+
   }
 )
 
+quantile_as_df <- function(vec, probs = c(0.025, 0.25, 0.5, 0.75, 0.975)) {
+
+  out <- data.frame(val = quantile(vec, probs), check.names = FALSE)
+  out <- tibble::rownames_to_column(out, "var")
+  out <- rbind(
+    out,
+    data.frame(var = "mu", val = mean(vec)),
+    data.frame(var = "sd", val = sd(vec))
+  )
+  out
+}
+
+compare <- pmap(
+  param_grid,
+  function(params_inf, params_inc, params_pinv) {
+    infile <- paste(
+      params_inf, params_inc, params_pinv, sep = "_"
+    )
+    simulated <- readRDS(
+      glue::glue("stanfits/simulated_{infile}.rds")
+    )
+    posterior <- readRDS(
+      glue::glue("stanfits/posterior_si_{infile}.rds")
+    )
+    simulated_summary <- quantile_as_df(simulated)
+    simulated_summary$category <- "Simulated"
+    posterior_summary <- quantile_as_df(posterior[[3]])
+    posterior_summary$category <- "Posterior"
+    rbind(simulated_summary, posterior_summary)
+})
+
+compare <- dplyr::bind_rows(compare, .id = "sim")
+
+compare <- tidyr::spread(compare, var, val)
+compare$sim <- factor(compare$sim, levels = 1:18, ordered = TRUE)
+
+p <- ggplot(compare) +
+  geom_point(
+    aes(sim, `50%`, col = category),
+    size = 2,
+    position = position_dodge(width = 0.3)
+  ) +
+  geom_linerange(
+    aes(sim, ymin = `2.5%`, ymax = `97.5%`, col = category),
+    size = 1.1,
+    position = position_dodge(width = 0.3)
+  ) +
+  theme_minimal() +
+  theme(legend.position = "top", legend.title = element_blank()) +
+  xlab("Simulation") +
+  ylab("Serial Interval (Median and 95% CrI)")
+
+ggsave("figures/scenario1a_mix_multiple_sim.png", p)
