b48ad0b17703543b5a034425941d3a1d904db1be
sangeeta <sangeetabhatia03@gmail.com>
2020-11-12 23:10:07 +0000

    4a looking alright

--- fit_to_simualted.R
+++ fit_to_simualted.R
@@ -29,20 +29,70 @@ fits_1a <- stan(
   verbose = TRUE
 )
 
-best_params <- map_estimates(fits_1a)
 
-posterior_si <- simulate_si(
-  params[["inc_par1"]][["mean_inc"]],
-  params[["inc_par1"]][["sd_inc"]],
-  shape1 = best_params[["alpha1"]],
-  shape2 = best_params[["beta1"]],
-  max_shed, NULL, NULL, nsim = 10000
+
+sim_data <- readRDS("data/simulated_1.rds")
+sim_data <- sim_data[sim_data$type == "valid filtered", ]
+sim_data <- sim_data[sample(nrow(sim_data), nsim), ]
+width <- min(sim_data$si[sim_data$si > 0]) / 2
+fits_4a <- stan(
+  file = here::here("stan-models/scenario4a.stan"),
+  data = list(
+    N = nrow(sim_data),
+    si = sim_data$si,
+    nu = sim_data$nu,
+    max_shed = max_shed,
+    alpha2 = params_inc[["shape"]],
+    beta2 = 1 / params_inc[["scale"]],
+    offset1 = offset,
+    width = 0.1
+  ),
+  chains = 3,
+  iter = 2000,
+  verbose = TRUE
+)
+
+## out <- pmap(
+##   sim_data,
+##   function(type, si, nu) {
+##     if (si < 0 | nu < 0) return(NULL)
+##     else {
+##       scenario4a_lpdf(
+##         si, nu, max_shed, offset, params_inf[[1]], params_inf[[2]],
+##         params_inc[["shape"]], 1 / params_inc[["scale"]], width
+##       )
+##     }
+##   }
+## )
+
+best_params <- map_estimates(fits_4a)
+
+params_inc <- epitrix::gamma_mucv2shapescale(
+  mu = params_inc[[1]], cv = params_inc[[2]] / params_inc[[1]]
+)
+params_inc$rate <- 1 / params_inc$scale
+
+posterior_si <- better_simulate_si(
+  params_inc,
+  list(shape1 = best_params[["alpha1"]], shape2 = best_params[["beta1"]]),
+  params_iso, offset, max_shed, 10000
 )
 
 ggplot() +
-  geom_density(data = sim_data, aes(si), fill = "red", alpha = 0.3) +
-  geom_density(data = posterior_si, aes(si), fill = "blue", alpha = 0.3) +
-  theme_minimal()
+  geom_density(
+    data = sim_data, aes(si, fill = "red"), alpha = 0.3, col = NA
+  ) +
+  geom_density(
+    data = posterior_si, aes(si, fill = "blue"),
+    alpha = 0.3, col = NA
+  ) +
+  scale_fill_identity(
+    breaks = c("red", "blue"),
+    labels = c("Simulated", "Posterior"),
+    guide  = "legend"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "top", legend.title = element_blank())
 
 true_params <- beta_muvar2shape1shape2(
   params[["inf_par1"]][["mean_inf"]] / max_shed,
@@ -53,6 +103,17 @@ inf_draws <- max_shed *
   rbeta( 10000, true_params$shape1, true_params$shape2)
 
 ggplot() +
-  geom_density(data = NULL, aes(inf_draws), fill = "red", alpha = 0.3) +
-  geom_density(data = posterior_si, aes(t_1), fill = "blue", alpha = 0.3) +
-  theme_minimal()
+  geom_density(
+    data = NULL, aes(inf_draws, fill = "red"),
+    alpha = 0.3, col = NA) +
+  geom_density(
+    data = posterior_si, aes(t_1, fill = "blue"), alpha = 0.3, col = NA
+  ) +
+  scale_fill_identity(
+    breaks = c("red", "blue"),
+    labels = c("Simulated", "Posterior"),
+    guide  = "legend"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "top", legend.title = element_blank())
+
--- stan-models/scenario4a.stan
+++ stan-models/scenario4a.stan
@@ -10,20 +10,16 @@ data{
   real <lower = 0> width;
 }
 parameters{
-  real <lower = 0, upper = 30> alpha1; // infectious profile parameter
-  real <lower = 0, upper = 30> beta1;  // infectious profile parameter
+  real <lower = 0, upper = 100> alpha1; // infectious profile parameter
+  real <lower = 0, upper = 100> beta1;  // infectious profile parameter
 }
 model{
   //print("alpha1 ", alpha1);
   //print("beta1 ", beta1);
   for (n in 1:N) {
-    if (si[n] > 0) {
       target += scenario4a_lpdf(si[n]| nu[n], max_shed, offset1, alpha1,
                                 beta1, alpha2, beta2, width);
       //print("target = ", target());
 
-    } else {
-      target += 0;
-    }  
   }
 }
