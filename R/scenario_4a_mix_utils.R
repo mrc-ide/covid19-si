d08e8ede01eb0aef292dc6950d30dc135b354526
AmyDighe <amydighe@gmail.com>
2021-01-24 21:48:58 +0000

    ensured final dataset is of size nsim

--- R/scenario_4a_mix_utils.R
+++ R/scenario_4a_mix_utils.R
@@ -3,7 +3,7 @@ simulate_4a_mix <- function(params_inc, params_inf, params_iso,
                             alpha_invalid, beta_invalid, min_si, max_si,
                             p_recall) {
   
-  nsim_local <- 10*nsim
+  nsim_local <- 100*nsim
   
   valid_si <- better_simulate_si(
     params_inc, params_inf, params_iso,
@@ -14,11 +14,13 @@ simulate_4a_mix <- function(params_inc, params_inf, params_iso,
     dplyr::select(si, nu)
   valid_si$group <- "valid"
   
-  valid_si <- valid_si[sample(nrow(valid_si), nsim), ]
+  nsim_local2 <- 10*nsim
+  
+  valid_si <- valid_si[sample(nrow(valid_si), nsim_local2), ]
   
   invalid_si <- data.frame(
-    si = rbeta(nsim, shape1 = alpha_invalid, shape2 = beta_invalid),
-    nu = rgamma(nsim, shape = params_iso$shape, scale = params_iso$scale)
+    si = rbeta(nsim_local2, shape1 = alpha_invalid, shape2 = beta_invalid),
+    nu = rgamma(nsim_local2, shape = params_iso$shape, scale = params_iso$scale)
   )
   f <- map_into_interval(0, 1, min_si, max_si)
   invalid_si$si <- f(invalid_si$si)
@@ -26,7 +28,7 @@ simulate_4a_mix <- function(params_inc, params_inf, params_iso,
   
   simulated_si <- valid_si[0, ]
   
-  for (idx in seq_len(nsim)) {
+  for (idx in seq_len(nsim_local2)) {
     toss <- runif(1)
     if (toss <= pinvalid) {
       simulated_si <- rbind(
@@ -40,17 +42,25 @@ simulate_4a_mix <- function(params_inc, params_inf, params_iso,
   }
   
   # sample using probability of recall
-  simulated_si$toss2 <- runif(nsim, min = 0, max = 1)
+  simulated_si$toss2 <- runif(nsim_local2, min = 0, max = 1)
   
   simulated_si_recalled <- simulated_si%>%
     dplyr::mutate(recall = exp(-abs(si-nu)*p_recall))%>%
     dplyr::filter(toss2 < recall)
   
+  simulated_si_recalled <- simulated_si_recalled[sample(nrow(simulated_si_recalled), nsim),]
+  
   list(
     valid_si = valid_si,
     invalid_si = invalid_si,
-    simulated_si = simulated_si,
-    simulated_si_recalled = simulated_si_recalled
+    simulated_si = simulated_si[,c("si", "nu", "group")],
+    simulated_si_recalled = simulated_si_recalled[,c("si", "nu", "group")]
   )
 }
 
+test <- simulate_4a_mix(params_inc = params_inc_all[[1]], 
+                        params_inf = params_inf_all[[1]], 
+                        params_iso = params_iso_all[[1]],
+                            offset = -2, max_shed = 21, pinvalid = 0, nsim = 600,
+                            alpha_invalid = 1, beta_invalid = 1, min_si = -2, max_si = 40,
+                            p_recall = 0.5)
