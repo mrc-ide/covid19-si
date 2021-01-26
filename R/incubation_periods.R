aad277288a941628e0cef90b07fb4c5b1b367871
AmyDighe <amydighe@gmail.com>
2021-01-26 17:36:00 +0000

    comparing incubation periods

---
+++ R/incubation_periods.R
@@ -0,0 +1,18 @@
+# plot incubation period:
+
+x <- seq(0, 30, by = 0.1)
+
+IBM_inc <- dgamma(x = x, 
+                  shape = 1.675513,
+                  scale = 3.043843)
+
+Lauer_inc <- dgamma(x = x, 
+                    shape = 5.807,
+                    scale = 0.948)
+
+inc_df <- data.frame(x, IBM_inc, Lauer_inc)
+
+inc_df <- reshape2::melt(inc_df, id = "x")
+
+ggplot(inc_df, aes(x = x, y = value, group = variable))+
+  geom_line(aes(col = variable))
