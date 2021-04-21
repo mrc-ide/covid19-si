1c0c20894c096cd7c271b074bd3fd27038068c3c
AmyDighe <amydighe@gmail.com>
2021-04-21 11:26:18 +0100

    visualising relationship between importation and si

---
+++ R/importation_descriptive_analysis.R
@@ -0,0 +1,25 @@
+
+data <- readRDS("data/cowling_data_clean.rds")
+
+data <- data%>%
+  mutate(delay_import = infector_returnDate_fromOtherCity - infector_onsetDate)
+
+# relationship between SI and importation (Y/N)
+dplyr::count(data, infector_returned_fromOtherCity)
+
+ggplot(data)+
+  geom_boxplot(aes(x = infector_returned_fromOtherCity, y = si))
+
+# relationship between SI and importation (days)
+ggplot(data%>%filter(infector_returned_fromOtherCity == "imported"))+
+  geom_point(aes(x = delay_import, y = si))#+
+  #geom_abline(slope = 1, intercept = 0)
+
+# relationship between importation (Y/N) and time
+ggplot(data)+
+  geom_boxplot(aes(x = infector_returned_fromOtherCity, y = infector_onsetDate))
+
+# relationship between importation (days) and time
+ggplot(data%>%filter(infector_returned_fromOtherCity == "imported"))+
+  geom_point(aes(x = delay_import, y = infector_onsetDate))+
+  geom_abline(slope = 1, intercept = 0)
