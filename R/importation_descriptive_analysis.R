2e805199da5b3bbdd555961e414b8cb841ce8bd8
AmyDighe <amydighe@gmail.com>
2021-04-21 22:24:06 +0100

    initial commit of processfits

--- R/importation_descriptive_analysis.R
+++ R/importation_descriptive_analysis.R
@@ -14,8 +14,6 @@ ggplot(data)+
 ggplot(data%>%filter(infector_returned_fromOtherCity == "imported"), aes(x = delay_import, y = si))+
   geom_point()+
   geom_smooth(method='lm')
-  #+
-  #geom_abline(slope = 1, intercept = 0)
 
 # relationship between importation (Y/N) and time
 ggplot(data)+
@@ -30,3 +28,17 @@ ggplot(data%>%filter(infector_returned_fromOtherCity == "imported"))+
 data %>%
   group_by(infector_returned_fromOtherCity) %>%
   dplyr::summarize(Mean = mean(onset_first_iso, na.rm=TRUE))
+
+ggplot(data)+
+  geom_boxplot(aes(x = infector_returned_fromOtherCity, y = onset_first_iso))
+
+#plot date of isolation and date of importation
+
+data <- data%>%
+  mutate(first_iso = pmin(infector_isolateDate_beforeSymptom, infector_isolateDate_afterSymptom, na.rm = TRUE))
+
+ggplot(data)+
+  geom_point(aes(x = infector_returnDate_fromOtherCity, y = first_iso))+
+  geom_abline(slope = 1, intercept = 0)
+
+sum(data$first_iso == data$infector_returnDate_fromOtherCity, na.rm = TRUE)
