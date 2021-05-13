2348a13bb0f469253014338a299aacb23402c9d8
sangeeta <sangeetabhatia03@gmail.com>
2021-05-13 13:24:30 +0100

    Smaller steps
    so that we don't have to repeat upstream tasks if something goes wrong

--- R/process_nf_fits.R
+++ R/process_nf_fits.R
@@ -34,64 +34,116 @@ model_features <- model_features[index, ]
 
 if (! dir.exists("processed_stanfits")) dir.create("processed_stanfits")
 
-pwalk(
-  model_features,
-  function(mixture, left_bias, recall, right_bias, model_prefix) {
+fits <- map(
+  model_features$model_prefix,
+  function(model_prefix) {
+    readRDS(glue("stanfits/{model_prefix}_nf_fit.rds"))
+  }
+)
 
-    fit <- readRDS(glue("stanfits/{model_prefix}_nf_fit.rds"))
+table1 <- map(
+  fits,
+  function(fit) {
     tab1 <- fitted_params(fit)
     saveRDS(
       tab1, glue("processed_stanfits/{model_prefix}_nf_tab1.rds")
     )
+    tab1
+  }
+)
 
-    samples_tost <- estimated_TOST_nf(
+
+samples_tost <- map2(
+  table1, fits, function(tab1, fit) {
+    estimated_TOST_nf(
       tab1, taus = seq(-20, 40, 0.1), n = 1e4,
       rstan::extract(fit)
     )
-    best_si <- estimated_SI(
-      cowling_data, samples_tost$TOST_bestpars, mixture = mixture,
-      recall = recall, isol = right_bias, tab1 = tab1
-    )
+  }
+)
 
-    mean_si <- estimated_SI(
-      cowling_data, samples_tost$TOST_meanpars, mixture = mixture,
+best_si <- pmap(
+  list(
+    tost = samples_tost, mixture = model_features$mixture,
+    recall = model_features$recall, isol = model_features$right_bias,
+    tab1 = table1
+  ), function(tost, mixture, recall, isol, tab1) {
+    estimated_SI(
+      cowling_data, tost$TOST_bestpars, mixture = mixture,
       recall = recall, isol = right_bias, tab1 = tab1
-    )
-
-    post_si <- apply(
-      samples_tost$TOST_post, 2, function(inf_samples) {
-        estimated_SI(
-          cowling_data, inf_samples, mixture = mixture,
-          recall = recall, isol = right_bias, tab1 = tab1_s3
-        )
-      })
-    ## Extract conditional SI and make a matrix/data.frame
-    post_si_s3_mat <- map(post_si, ~ .[[2]]) %>%
-      do.call(what = 'rbind')
-    ## Use the same names so that Amy's code can be re-used
-    samples_si <- list(
-      SI_meanpars = list(SI = mean_si[[2]]),
-      SI_bestpars = list(SI = best_si[[2]]),
-      SI_post = post_si_s3_mat
-    )
-    ## table 2 - summary stats for sampled distributions
-    tab2 <- tost_si_summary(samples_tost, samples_si)
-    saveRDS(
-      tab2, glue("processed_stanfits/{model_prefix}_nf_tab2.rds")
-    )
-    p1 <- TOST_figure(samples_tost$TOST_bestpars)
-    save_plot(
-      filename = glue("figures/{model_prefix}_nf_tost.png"), p1
-    )
-    psi <- SI_figure(samples_si$SI_bestpars$SI, cowling_data)
-    save_plot(
-      filename = glue("figures/{model_prefix}_nf_si.png"), psi
-    )
-    psi2 <- plot_both_SIs(
-      SI1 = best_si[[1]], SI2 = best_si[[2]], data = cowling_data
-    )
-    save_plot(
-      filename = glue("figures/{model_prefix}_nf_si.png"), psi2
     )
   }
 )
+
+mean_si <- pmap(
+  list(
+    tost = samples_tost, mixture = model_features$mixture,
+    recall = model_features$recall, isol = model_features$right_bias,
+    tab1 = table1
+  ), function(tost, mixture, recall, isol, tab1) {
+    estimated_SI(
+      cowling_data, tost$TOST_meanpars, mixture = mixture,
+      recall = recall, isol = right_bias, tab1 = tab1
+    )
+  }
+)
+
+post_si <- pmap(
+  list(
+    tost = samples_tost, mixture = model_features$mixture,
+    recall = model_features$recall, isol = model_features$right_bias,
+    tab1 = table1
+  ), function(tost, mixture, recall, isol, tab1) {
+    x <- apply(
+      tost$TOST_post, 2, function(inf_samples) {
+        estimated_SI(
+          cowling_data, inf_samples, mixture = mixture,
+          recall = recall, isol = right_bias, tab1 = tab1
+        )
+      }
+    )
+    ## Extract conditional SI and make a matrix/data.frame
+    map(x, ~ .[[2]]) %>% do.call(what = 'rbind')
+  }
+)
+
+table2 <- pmap(
+  list(
+    mean_si = mean_si, best_si = best_si, post_si = post_si,
+    tost = samples_tost), function(mean_si, best_si, post_si, tost) {
+      samples_si <- list(
+        SI_meanpars = list(SI = mean_si[[2]]),
+        SI_bestpars = list(SI = best_si[[2]]),
+        SI_post = post_si
+      )
+      ## table 2 - summary stats for sampled distributions
+      tab2 <- tost_si_summary(tost, samples_si)
+      saveRDS(
+        tab2, glue("processed_stanfits/{model_prefix}_nf_tab2.rds")
+      )
+      tab2
+    }
+)
+
+walk(samples_tost, function(tost) {
+  p1 <- TOST_figure(tost$TOST_bestpars)
+  save_plot(
+    filename = glue("figures/{model_prefix}_nf_tost.png"), p1
+  )
+})
+
+
+walk(best_si, function(si) {
+  psi <- SI_figure(si[[2]], cowling_data)
+  save_plot(
+    filename = glue("figures/{model_prefix}_nf_si.png"), psi
+  )
+  psi2 <- plot_both_SIs(
+    SI1 = best_si[[1]], SI2 = best_si[[2]], data = cowling_data
+  )
+  save_plot(
+    filename = glue("figures/{model_prefix}_nf_si.png"), psi2
+  )
+})
+
+
