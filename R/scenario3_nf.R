a2bba1e0381c54c442d0a3a5fe8adaba74cb3ba2
sangeeta <sangeetabhatia03@gmail.com>
2021-05-20 13:24:01 +0100

    Leaky doesn't go with S3

--- R/scenario3_nf.R
+++ R/scenario3_nf.R
@@ -88,33 +88,3 @@ saveRDS(
 ggmcmc(
   ggs(fit_3a), glue::glue("checks/{prefix}_check.pdf")
 )
-
-
-
-prefix <- "scenario3a_leaky_nf"
-infile <- glue::glue("stan-models/{prefix}.stan")
-params_inc <- params_real$inc_par2
-si_vec <- seq(-20, max_valid_si)
-
-fit_3a <- stan(
-  file = here::here(infile),
-  data = list(
-    N = nrow(cowling_data),
-    si = cowling_data$si,
-    max_shed = max_shed,
-    alpha2 = params_inc[["shape"]],
-    beta2 = 1 / params_inc[["scale"]],
-    M = length(si_vec),
-    si_vec = si_vec,
-    width = 0.1
-  ),
-  verbose = TRUE
-)
-
-saveRDS(
-  fit_3a, glue::glue("stanfits/{prefix}_fit.rds")
-)
-
-ggmcmc(
-  ggs(fit_3a), glue::glue("checks/{prefix}_check.pdf")
-)
