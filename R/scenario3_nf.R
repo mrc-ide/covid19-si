61115dc65a360abb84ccfb86db75abfccc9fa06a
sangeeta <sangeetabhatia03@gmail.com>
2021-05-21 14:25:37 +0100

    Set-up for cluster

--- R/scenario3_nf.R
+++ R/scenario3_nf.R
@@ -1,28 +1,4 @@
-params_inc <- params_real$inc_par2
-si_vec <- seq(-20, max_valid_si)
-standata <- list(
-  N = nrow(cowling_data), si = cowling_data$si, max_shed = max_shed,
-  alpha2 = params_inc[["shape"]], beta2 = 1 / params_inc[["scale"]],
-  M = length(si_vec), si_vec = si_vec, width = 0.5
-)
+fits <- pmap(model_features, fit_model)
 
-fits <- pmap(
-  model_features, function(mixture, recall, right_bias, model_prefix) {
-    prefix <- glue("{model_prefix}_nf")
-    infile <- glue("stan-models/{prefix}.stan")
-    message(infile)
-    if (!file.exists(infile)) message("Does not exist ", infile)
-    if(mixture) {
-      standata$max_invalid_si <- max_invalid_si
-      standata$min_invalid_si <- min_invalid_si
-    }
-    if (right_bias) standata$nu <- cowling_data$nu
-
-    fit <- stan(
-      file = infile, data = standata,  verbose = FALSE, iter = iter,
-      chains = 1
-    )
-    saveRDS(fit, glue("test/{prefix}.rds"))
-    fit
-  }
-)
+## On the cluster
+## fits <- obj$enqueue_bulk(model_features, fit_model)
