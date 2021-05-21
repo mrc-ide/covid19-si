9a6f253229ca332a0bf8c08914700a93a9658768
sangeeta <sangeetabhatia03@gmail.com>
2021-05-21 14:15:43 +0100

    Fit all models in 1 loop

--- R/scenario3_nf.R
+++ R/scenario3_nf.R
@@ -3,13 +3,14 @@ si_vec <- seq(-20, max_valid_si)
 standata <- list(
   N = nrow(cowling_data), si = cowling_data$si, max_shed = max_shed,
   alpha2 = params_inc[["shape"]], beta2 = 1 / params_inc[["scale"]],
-  M = length(si_vec), si_vec = si_vec, width = 0.1
+  M = length(si_vec), si_vec = si_vec, width = 0.5
 )
 
 fits <- pmap(
   model_features, function(mixture, recall, right_bias, model_prefix) {
     prefix <- glue("{model_prefix}_nf")
     infile <- glue("stan-models/{prefix}.stan")
+    message(infile)
     if (!file.exists(infile)) message("Does not exist ", infile)
     if(mixture) {
       standata$max_invalid_si <- max_invalid_si
@@ -18,9 +19,10 @@ fits <- pmap(
     if (right_bias) standata$nu <- cowling_data$nu
 
     fit <- stan(
-      file = infile, data = standata,  verbose = TRUE, iter = iter
+      file = infile, data = standata,  verbose = FALSE, iter = iter,
+      chains = 1
     )
-
+    saveRDS(fit, glue("test/{prefix}.rds"))
     fit
   }
 )
