24f027949054535e71c3a9911d0540de27d82aab
sangeeta <sangeetabhatia03@gmail.com>
2021-07-01 14:07:52 +0100

    Split global.R into 2
    don't need plotting libraries on the cluster

--- global.R
+++ global.R
@@ -1,41 +1,27 @@
 set.seed(42)
 library(cowplot)
-library(dplyr)
-library(epitrix)
 library(ggforce)
-library(glue)
 library(ggmcmc)
 library(ggplot2)
 library(magrittr)
 library(matrixStats)
 library(patchwork)
-library(purrr)
-library(rstan)
-library(tibble)
-source("cowling-data-prep.R")
-source("R/utils.R")
+source("setup.R")
 source("R/utils_process_fits_common.R")
 source("R/utils_process_nf_fits.R")
 source("R/utils_model_selection.R")
 source("R/utils_process_beta_fits.R")
-
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = FALSE)
 
-## Larger max_shed for NF distribution
-max_shed <- 21
 nsim_pre_filter <- 20000
 nsim_post_filter <- 300
 alpha_invalid <- 1
 beta_invalid <- 1
-min_invalid_si <- -11 ## Real has as large as -11
 max_si <- 40
-max_invalid_si <- 40
 max_valid_si <- 40
-width <- 0.1
 
 # additional params for sim data
-
 params <- list(
   inf_par1 = list(mean_inf = 2, sd_inf = 1), ## short
   inf_par2 = list(mean_inf = 8, sd_inf = 2), ## long
@@ -54,35 +40,7 @@ params <- list(
   recall3 = 1
 )
 
-# additional parameters for real data
 
-params_real <- list(
-  inc_par1 =  list(shape = 5.807, scale = 0.948), # Lauer et al gamma
-  inc_par2 = list(shape = 1.675513, scale = 3.043843),# IBM
-  inc_par3 = list(shape = 5.807, scale = 0.948), # long - to be changed
-  offset1 = -1,
-  offset2 = -2,
-  offset3 = -3,
-  maxshed1 = 9,
-  maxshed2 = 14,
-  maxshed3 = 21
-)
-
-
-## Further investigation.
-##  distribution parameters assume max_shed = 21
-params_check <- list(
-  inf_par1 = list(mean_inf = 1.5, sd_inf = 0.5), ## short (comparable to s3 real data)
-  inf_par2 = list(mean_inf = 5, sd_inf = 5), ## long (comparable to s4 real data)
-  inc_par1 = list(shape = 4, scale = 0.5), ## short
-  inc_par2 = list(shape = 1.675513, scale = 3.043843),# IBM
-  offset1 = -3,
-  offset2 = -6,
-  pinvalid1 = 0.05,
-  pinvalid2 = 0.2,
-  beta1 = 0,
-  iso_par1 = list(shape = 1, scale = 5)
-)
 
 
 
@@ -98,108 +56,3 @@ params_check <- list(
 ## nf_lpdf(1, 1, 1, 10, 5)
 ## 1 / cosh(10 - 5)
 
-model_features <- list(
-  "mixture" = c(TRUE, FALSE),
-  ##"left_bias" = c(TRUE, FALSE),
-  "recall"  = c(TRUE, FALSE),
-  "right_bias" = c(TRUE, FALSE)
-)
-model_features <- expand.grid(model_features)
-model_features$model_prefix <- ifelse(
-  model_features$`right_bias`, "scenario4a", "scenario3a"
-)
-
-model_features$model_prefix <-ifelse(
-  model_features$mixture,
-  glue::glue("{model_features$model_prefix}_mixture"),
-  model_features$model_prefix
-)
-
-## model_features$model_prefix <-ifelse(
-##   model_features$`left_bias`,
-##   glue::glue("{model_features$model_prefix}_leftbias"),
-##   model_features$model_prefix
-## )
-
-model_features$model_prefix <-ifelse(
-  model_features$`recall`,
-  glue::glue("{model_features$model_prefix}_recall"),
-  model_features$model_prefix
-)
-
-short_run <- FALSE
-iter <- ifelse(short_run, 100, 3000)
-chains <- ifelse(short_run, 1, 2)
-
-params_inc <- params_real$inc_par2
-si_vec <- seq(-20, max_valid_si)
-## For s3/s4 mix
-## cowling_data <- data_s3_s4mix
-s3data <- list(
-  N = nrow(cowling_data), si = cowling_data$si, max_shed = max_shed,
-  alpha2 = params_inc[["shape"]], beta2 = 1 / params_inc[["scale"]],
-  M = length(si_vec), si_vec = si_vec, width = 0.5
-)
-s3s4mix <- list(
-  N = nrow(data_s3_s4mix), si = data_s3_s4mix$si, max_shed = max_shed,
-  alpha2 = params_inc[["shape"]], beta2 = 1 / params_inc[["scale"]],
-  M = length(si_vec), si_vec = si_vec, width = 0.5
-)
-
-#######
-
-fit_model <- function(mixture, recall, right_bias, model_prefix, standata = s3data, obs = cowling_data) {
-  prefix <- glue("{model_prefix}_nf")
-  infile <- glue("stan-models/{prefix}.stan")
-  message(infile)
-  if (!file.exists(infile)) message("Does not exist ", infile)
-  if(mixture) {
-    standata$max_invalid_si <- max_invalid_si
-    standata$min_invalid_si <- min_invalid_si
-  }
-  if (right_bias| recall) standata$nu <- obs$nu
-  fit <- stan(
-    file = infile, data = standata,  verbose = FALSE, iter = iter,
-    chains = chains
-  )
-  outfile <- glue("stanfits/{prefix}_fit.rds")
-  fit
-}
-
-
-s3s4_model <- function(mixture, recall, right_bias, model_prefix, standata = s3s4mix, obs = data_s3_s4mix) {
-  prefix <- glue("{model_prefix}_nf")
-  infile <- glue("stan-models/{prefix}.stan")
-  message(infile)
-  if (!file.exists(infile)) message("Does not exist ", infile)
-  if(mixture) {
-    standata$max_invalid_si <- max_invalid_si
-    standata$min_invalid_si <- min_invalid_si
-  }
-  if (right_bias| recall) standata$nu <- obs$nu
-  fit <- stan(
-    file = infile, data = standata,  verbose = FALSE, iter = iter,
-    chains = chains
-  )
-  outfile <- glue("stanfits/{prefix}_fit.rds")
-  fit
-}
-
-
-fit_leaky_model <- function(mixture, recall, right_bias, model_prefix, standata = s3data, obs = cowling_data) {
-  prefix <- glue("{model_prefix}_leaky_nf")
-  infile <- glue("stan-models/{prefix}.stan")
-  message(infile)
-  if (!file.exists(infile)) message("Does not exist ", infile)
-  standata$nu <- obs$nu
-  if(mixture) {
-    standata$max_invalid_si <- max_invalid_si
-    standata$min_invalid_si <- min_invalid_si
-  }
-  fit <- stan(
-    file = infile, data = standata,  verbose = FALSE, iter = iter,
-    chains = chains
-  )
-  outfile <- glue("stanfits/{prefix}_fit.rds")
-  fit
-}
---
+++ setup.R
@@ -0,0 +1,130 @@
+library(dplyr)
+library(epitrix)
+library(glue)
+library(purrr)
+library(rstan)
+library(tibble)
+source("cowling-data-prep.R")
+source("R/utils.R")
+max_shed <- 21
+min_invalid_si <- -11 ## Real has as large as -11
+max_invalid_si <- 40
+width <- 0.5
+
+# additional parameters for real data
+params_real <- list(
+  inc_par1 =  list(shape = 5.807, scale = 0.948), # Lauer et al gamma
+  inc_par2 = list(shape = 1.675513, scale = 3.043843),# IBM
+  inc_par3 = list(shape = 5.807, scale = 0.948), # long - to be changed
+  offset1 = -1,
+  offset2 = -2,
+  offset3 = -3,
+  maxshed1 = 9,
+  maxshed2 = 14,
+  maxshed3 = 21
+)
+
+
+model_features <- list(
+  "mixture" = c(TRUE, FALSE),
+  ##"left_bias" = c(TRUE, FALSE),
+  "recall"  = c(TRUE, FALSE),
+  "right_bias" = c(TRUE, FALSE)
+)
+model_features <- expand.grid(model_features)
+model_features$model_prefix <- ifelse(
+  model_features$`right_bias`, "scenario4a", "scenario3a"
+)
+
+model_features$model_prefix <-ifelse(
+  model_features$mixture,
+  glue::glue("{model_features$model_prefix}_mixture"),
+  model_features$model_prefix
+)
+
+## model_features$model_prefix <-ifelse(
+##   model_features$`left_bias`,
+##   glue::glue("{model_features$model_prefix}_leftbias"),
+##   model_features$model_prefix
+## )
+
+model_features$model_prefix <-ifelse(
+  model_features$`recall`,
+  glue::glue("{model_features$model_prefix}_recall"),
+  model_features$model_prefix
+)
+
+short_run <- FALSE
+iter <- ifelse(short_run, 100, 3000)
+chains <- ifelse(short_run, 1, 2)
+
+params_inc <- params_real$inc_par2
+si_vec <- seq(-20, max_valid_si)
+## For s3/s4 mix
+## cowling_data <- data_s3_s4mix
+s3data <- list(
+  N = nrow(cowling_data), si = cowling_data$si, max_shed = max_shed,
+  alpha2 = params_inc[["shape"]], beta2 = 1 / params_inc[["scale"]],
+  M = length(si_vec), si_vec = si_vec, width = width
+)
+s3s4mix <- list(
+  N = nrow(data_s3_s4mix), si = data_s3_s4mix$si, max_shed = max_shed,
+  alpha2 = params_inc[["shape"]], beta2 = 1 / params_inc[["scale"]],
+  M = length(si_vec), si_vec = si_vec, width = width
+)
+
+#######
+
+fit_model <- function(mixture, recall, right_bias, model_prefix, standata = s3data, obs = cowling_data) {
+  prefix <- glue("{model_prefix}_nf")
+  infile <- glue("stan-models/{prefix}.stan")
+  message(infile)
+  if (!file.exists(infile)) message("Does not exist ", infile)
+  if(mixture) {
+    standata$max_invalid_si <- max_invalid_si
+    standata$min_invalid_si <- min_invalid_si
+  }
+  if (right_bias| recall) standata$nu <- obs$nu
+  fit <- stan(
+    file = infile, data = standata,  verbose = FALSE, iter = iter,
+    chains = chains
+  )
+  fit
+}
+
+
+s3s4_model <- function(mixture, recall, right_bias, model_prefix, standata = s3s4mix, obs = data_s3_s4mix) {
+  prefix <- glue("{model_prefix}_nf")
+  infile <- glue("stan-models/{prefix}.stan")
+  message(infile)
+  if (!file.exists(infile)) message("Does not exist ", infile)
+  if(mixture) {
+    standata$max_invalid_si <- max_invalid_si
+    standata$min_invalid_si <- min_invalid_si
+  }
+  if (right_bias| recall) standata$nu <- obs$nu
+  fit <- stan(
+    file = infile, data = standata,  verbose = FALSE, iter = iter,
+    chains = chains
+  )
+  fit
+}
+
+
+fit_leaky_model <- function(mixture, recall, right_bias, model_prefix, standata = s3data, obs = cowling_data) {
+  prefix <- glue("{model_prefix}_leaky_nf")
+  infile <- glue("stan-models/{prefix}.stan")
+  message(infile)
+  if (!file.exists(infile)) message("Does not exist ", infile)
+  standata$nu <- obs$nu
+  if(mixture) {
+    standata$max_invalid_si <- max_invalid_si
+    standata$min_invalid_si <- min_invalid_si
+  }
+  fit <- stan(
+    file = infile, data = standata,  verbose = FALSE, iter = iter,
+    chains = chains
+  )
+  fit
+}
+
