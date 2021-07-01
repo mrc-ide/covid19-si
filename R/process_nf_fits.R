24b2a0c6a69a64218fa9e952b631fc0ac2cae605
sangeeta <sangeetabhatia03@gmail.com>
2021-07-01 16:57:03 +0100

    Split into 2 files

--- R/process_nf_fits.R
+++ R/process_nf_fits.R
@@ -216,104 +216,12 @@ saveRDS(x, glue("{outdir}/nf_overall_table2.rds"))
 
 
 ## For manuscript
-for_ms <- select(x, model, mean_inf, sd_inf, mean_si, sd_si, DIC)
-for_ms <- arrange(for_ms, DIC)
-for_ms <- left_join(
-  for_ms, model_features, by = c("model" = "model_prefix")
-)
-for_ms <- select(
- for_ms, mixture, recall, right_bias, mean_inf, sd_inf, mean_si, sd_si, DIC
-)
+## for_ms <- select(x, model, mean_inf, sd_inf, mean_si, sd_si, DIC)
+## for_ms <- arrange(for_ms, DIC)
+## for_ms <- left_join(
+##   for_ms, model_features, by = c("model" = "model_prefix")
+## )
+## for_ms <- select(
+##  for_ms, mixture, recall, right_bias, mean_inf, sd_inf, mean_si, sd_si, DIC
+## )
 ## stargazer::stargazer(for_ms, summary = FALSE, rownames = FALSE)
-overall_table2 <- readRDS(
-  glue("{outdir}/nf_overall_table2.rds")
-)
-overall_table2 <- arrange(overall_table2, DIC)
-names(best_si) <- model_features$model_prefix
-## Reorder best_si
-best_unconditional <- map_dfr(
-  best_si, function(x) {
-    data.frame(si = x[["unconditional"]])
-  }, .id = "model"
-)
-
-best_unconditional$model <- factor(
-  best_unconditional$model, levels = overall_table2$model,
-  ordered = TRUE
-)
-## Top 4 moldes
-top4 <- best_unconditional[best_unconditional$model %in% overall_table2$model[1:4], ]
-palette <- c("#56B4E9", "#009E73", "#0072B2", "#D55E00")
-names(palette) <- overall_table2$model[1:4]
-
-nice_model_name <- function(model) {
-  prefix <- case_when(
-    grepl("scenario3", model) ~ "BASELINE",
-    grepl("scenario4", model) ~ "ISOL"
-  )
-  suffix1 <- case_when(
-    grepl("mixture", model) ~ " + MIX",
-    TRUE ~ ""
-  )
-  suffix2 <- case_when(
-    grepl("recall", model) ~ " + RECALL",
-    TRUE ~ ""
-  )
-
-  paste0(prefix, suffix1, suffix2)
-}
-
-p <- ggplot(top4, aes(model, si, fill = model)) +
-  gghalves::geom_half_violin(
-    draw_quantiles = c(0.25, 0.5, 0.75)
-    ) +
-  scale_fill_manual(
-    values = palette,
-    breaks = overall_table2$model[1:4],
-    labels = nice_model_name
-  ) +
-  theme_minimal() +
-  ylab("Serial Interval") +
-  theme(
-    axis.text.x = element_blank(),
-    axis.title.x = element_blank(),
-    legend.position = "top",
-    legend.title = element_blank(),
-  )
-
-ggsave(
-  glue("{figs_dir}/nf_top4_si_distr.pdf"), p
-)
-
-
-## Similary TOST
-names(samples_tost) <- model_features$model_prefix
-tost_bestpars <- map_dfr(
-  samples_tost, function(x) data.frame(tost = x[["TOST_bestpars"]]),
-  .id = "model"
-)
-tost_bestpars$model <- factor(
-  tost_bestpars$model, levels = overall_table2$model,
-  ordered = TRUE
-)
-
-p <- ggplot(tost_bestpars, aes(model, tost)) +
-  gghalves::geom_half_violin(
-    fill = "red", alpha = 0.3,
-    draw_quantiles = c(0.25, 0.5, 0.75)
-  ) +
-  ##geom_boxplot(width = 0.1, color = "grey", alpha = 0.5, coef = 1) +
-  theme_minimal() +
-  ylab("TOST") +
-  scale_x_discrete(
-    breaks = c("scenario4a_mixture", "scenario3a_mixture_recall", "scenario4a_mixture_recall",
-               "scenario4a_recall", "scenario3a_mixture", "scenario3a_recall"),
-    labels = c("ISOL + MIX", "BASELINE + MIX + RECALL", "ISOL + MIX + RECALL",
-               "ISOL + RECALL", "BASELINE + MIX", "BASELINE + RECALL")
-  ) +
-  theme(
-    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
-    axis.title.x = element_blank()
-  )
-
-ggsave("{figs_dir}/nf_all_tost_distr.pdf", p)
