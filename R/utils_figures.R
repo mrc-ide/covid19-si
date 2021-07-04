d716a684ee268c7c84f275e3679d6dc624337caf
sangeeta <sangeetabhatia03@gmail.com>
2021-07-04 13:54:46 +0100

    Harmonised palette

--- R/utils_figures.R
+++ R/utils_figures.R
@@ -15,51 +15,56 @@ nice_model_name <- function(model) {
   paste0(prefix, suffix1, suffix2)
 }
 
+palette <- c(
+  scenario3a = "#56B4E9", scenario4a = "#56B4E9",
+  scenario3a_mixture = "#009E73",
+  scenario4a_mixture = "#009E73",
+  scenario3a_recall = "#CC79A7",
+  scenario4a_recall = "#CC79A7",
+  scenario3a_mixture_recall = "#D55E00",
+  scenario4a_mixture_recall = "#D55E00"
+)
+
+
 outdir <- "processed_stanfits/discrete_pairs"
 figs_dir <- "figures/discrete_pairs"
 
 
 overall_table2 <- readRDS(glue("{outdir}/nf_overall_table2.rds"))
 overall_table2 <- arrange(overall_table2, DIC)
+
+best_si <- readRDS(glue("{outdir}/best_si_nf.rds"))
 names(best_si) <- model_features$model_prefix
 
+s3models <- c(
+  "scenario3a", "scenario3a_mixture",
+  "scenario3a_recall", "scenario3a_mixture_recall"
+)
+
 best_unconditional <- map_dfr(
-  overall_table2$model[1:4],
-  function(model) {
-    s3 <- ifelse(
-      grepl('scenario3', model),
-      model,
-      gsub('scenario4', 'scenario3', model)
-    )
-    s4 <- ifelse(
-      grepl('scenario3', model),
-      gsub('scenario4', 'scenario3', model),
-      model
-    )
+  s3models,
+  function(s3) {
+    s4 <- gsub('scenario3', 'scenario4', s3)
     data.frame(
       s3si = best_si[[s3]][["unconditional"]],
       s4si = best_si[[s4]][["unconditional"]],
-      model = model
+      model = s3
     )
   }
 )
 
 best_unconditional$model <- factor(
-  best_unconditional$model, levels = overall_table2$model[1:4],
+  best_unconditional$model,
+  levels = s3models,
   ordered = TRUE
 )
-## Top 4 models
-##s3models <- grep('scenario3', overall_table2$model, value = TRUE)
-##top4 <- best_unconditional[best_unconditional$model %in% overall_table2$model[1:4], ]
-palette <- c("#56B4E9", "#009E73", "#CC79A7", "#D55E00")
-names(palette) <- overall_table2$model[1:4]
 
 
 p <- ggplot(best_unconditional) +
   gghalves::geom_half_violin(
     aes(model, s3si, fill = model),
     draw_quantiles = c(0.25, 0.5, 0.75), side = "l"
-    ) +
+  ) +
   gghalves::geom_half_violin(
     aes(model, s4si, fill = model),
     draw_quantiles = c(0.25, 0.5, 0.75), side = "r",
@@ -67,8 +72,8 @@ p <- ggplot(best_unconditional) +
   ) +
   scale_fill_manual(
     values = palette,
-    breaks = overall_table2$model[1:4],
-    labels = nice_model_name(overall_table2$model[1:4]),
+    breaks = levels(best_unconditional$model),
+    labels = nice_model_name,
     guide = guide_legend(nrow = 2, byrow = TRUE)
   ) +
   theme_minimal() +
@@ -80,29 +85,21 @@ p <- ggplot(best_unconditional) +
     legend.title = element_blank(),
   )
 
-ggsave(glue("{figs_dir}/nf_top4_si_distr.pdf"), p)
+ggsave(glue("{figs_dir}/nf_top4_si_distr.png"), p)
 
 
 ## Similary TOST
+samples_tost <- readRDS(glue("{outdir}/samples_tost_nf.rds"))
 names(samples_tost) <- model_features$model_prefix
 
 tost_bestpars <- map_dfr(
-  overall_table2$model[1:4],
-  function(model) {
-    s3 <- ifelse(
-      grepl('scenario3', model),
-      model,
-      gsub('scenario4', 'scenario3', model)
-    )
-    s4 <- ifelse(
-      grepl('scenario3', model),
-      gsub('scenario4', 'scenario3', model),
-      model
-    )
+  s3models,
+  function(s3) {
+    s4 <- gsub('scenario3', 'scenario4', s3)
     data.frame(
       s3tost = samples_tost[[s3]][["TOST_bestpars"]],
       s4tost = samples_tost[[s4]][["TOST_bestpars"]],
-      model = model
+      model = s3
     )
   }
 )
@@ -119,8 +116,8 @@ p <- ggplot(tost_bestpars) +
   ) +
   scale_fill_manual(
     values = palette,
-    breaks = overall_table2$model[1:4],
-    labels = nice_model_name(overall_table2$model[1:4]),
+    breaks = levels(best_unconditional$model),
+    labels = nice_model_name,
     guide = guide_legend(nrow = 2, byrow = TRUE)
   ) +
   theme_minimal() +
@@ -132,6 +129,4 @@ p <- ggplot(tost_bestpars) +
     legend.title = element_blank(),
   )
 
-ggsave(
-  glue("{figs_dir}/nf_all_tost_distr.pdf"), p
-)
+ggsave(glue("{figs_dir}/nf_all_tost_distr.png"), p)
