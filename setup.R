d11ecb11895124be65702075c358424e115af06b
sangeeta <sangeetabhatia03@gmail.com>
2021-07-05 17:24:56 +0100

    Max-shed 21 again to fit models with Neil's priors

--- setup.R
+++ setup.R
@@ -6,10 +6,10 @@ library(rstan)
 library(tibble)
 source("cowling-data-prep.R")
 source("R/utils.R")
-max_shed <- 28
+max_shed <- 21
 min_valid_si <- -20
 max_valid_si <- 40
-min_invalid_si <- -20 
+min_invalid_si <- -20
 max_invalid_si <- 40
 width <- 0.5
 
