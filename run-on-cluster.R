d23459f0449104204d7eb4e2fefea527475bfd96
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2021-07-09 12:33:29 +0100

    Runs with s3s4mix and discrete pairs

--- run-on-cluster.R
+++ run-on-cluster.R
@@ -38,6 +38,7 @@ fits_pairs <- obj$enqueue_bulk(model_features, fit_model_to_pairs)
 # 01.07.2021 Fit all models to discrete pairs only 'premythical_americankestrel'
 # 03.07.2021 Sensitivity analyses: max-shed 28 'sporadic_goshawk'
 # 05.07.2021 max shed 21 and Neil's priors 'zoometrical_xantusmurrelet'
-fits <- obj$task_bundle_get('zoometrical_xantusmurrelet')
+# 08.07.2021 max shed 21 and Neil's priors s3s4mix, discrete pairs operose_anemonecrab
+fits <- obj$task_bundle_get('operose_anemonecrab')
 outfiles <- glue('stanfits/maxshed21_nfpriors/discrete_pairs/{model_features$model_prefix}_nf_fit.rds')
 purrr::walk2(fits$results(), outfiles, function(x, y) saveRDS(x, y))
